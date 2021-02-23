f9277792ab37fa5189f81b65c96b2a765868338c
ntdunkley <n.dunkley@protonmail.com>
2021-02-23 21:16:01 +0200

    Initial commit

---
+++ src/main/java/com/nathan/springrestfularticles/SpringRestfulArticlesApplication.java
@@ -0,0 +1,13 @@
+package com.nathan.springrestfularticles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestfulArticlesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestfulArticlesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nathan/springrestfularticles/SpringRestfulArticlesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nathan.springrestfularticles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestfulArticlesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
